bc132546df7564902712d50d473f722e4ef3235b
Anand Kumar Kurapati <anand.kurapati@swiggy.in>
2017-12-09 19:21:01 +0530

    Twitter config

--- src/main/java/appengine/parser/MainController.java
+++ src/main/java/appengine/parser/MainController.java
@@ -8,14 +8,21 @@ import appengine.parser.facebook.AcceptFriendRequests;
 import appengine.parser.facebook.DetailedPost;
 import appengine.parser.facebook.Facebook;
 import appengine.parser.objects.AccessToken;
+import appengine.parser.objects.twitter4j.Tweet;
+import appengine.parser.twitter.Twitter;
 import appengine.parser.utils.ConstantsData;
 import appengine.parser.utils.ConstantsFunctions;
 import appengine.parser.utils.Log;
+import appengine.parser.utils.StringUtils;
+import appengine.parser.wordpress.WordPress;
 import com.restfb.types.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import twitter4j.QueryResult;
+import twitter4j.Status;
 
 import java.util.ArrayList;
 
@@ -66,6 +73,30 @@ public class MainController {
     @GetMapping("/acceptfriendrequests")
     public String acceptFriendRequests() {
         AcceptFriendRequests.acceptFriendRequestsParallelly();
+        return "{\"success\"}";
+    }
+
+    @GetMapping("/getfromtwittertowpfb/{search}")
+    public String getFromTwitterToWPFB(@PathVariable String search){
+        try {
+            QueryResult queryResult = Twitter.getTweets(search);
+            ArrayList<Tweet> publishedTweets = new ArrayList<Tweet>();
+            for (Status status : queryResult.getTweets()) {
+                Tweet tweet = new Tweet(status);
+                if (tweet.isWithImage() && !tweet.similarTweetExists(publishedTweets)) {
+                    publishedTweets.add(tweet);
+                    String wp_post_link = WordPress.publish(tweet.getFormattedTweet(), tweet.getOEmbedUrl(Twitter.getInstance()), tweet.getImageUrl());
+                    if (StringUtils.isNonEmpty(wp_post_link))
+                        Facebook.publish(tweet.getFormattedTweet(), wp_post_link, tweet.getImageUrl());
+                }
+            }
+            if(publishedTweets.size()==0){
+                queryResult.getSinceId();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "{\"success\"}";
     }
 
